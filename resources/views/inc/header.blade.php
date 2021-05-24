3249dd206d7cff63939d7d20ac340756806cfbcc
DenysMatusev <turtleluck@gmail.com>
2021-05-24 22:09:59 +0300

    Inital Commit

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>PHP TEST</title>
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto|Varela+Round'>
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
+  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+  <link rel="stylesheet" href="{{ url('css/style.css') }}">
+  <script type="text/javascript" src="{{ url('js/index.js') }}"></script>
+</head>
+
+<body>
+  <div style="text-align: -webkit-right;">
+    <ul style="width: 70%">
+      <li><a class="active" href="#home">Aerial Work Platforms</a></li>
+      <li><a href="#news">Air Compressors & Air Tools</a></li>
+      <li><a href="#contact">Concrete Masonry</a></li>
+      <li><a href="#about">Earthmoving Equipment</a></li>
+    </ul>
+  </div>
+</body>
